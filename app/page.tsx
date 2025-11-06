614381327308bdefdc3cce870de00eca0853ae32
Iga Tunde <igatunde06@gmail.com>
2025-11-06 03:21:25 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import ExploreBtn from "./components/ExploreBtn";
+
 const Page = () => {
   return (
-    <div><h1>Welcome to my homePage</h1></div>
-  )
-}
+    <section>
+      <h1 className="text-center">
+        The Hub For Every Dev <br /> Event You Can`t Miss
+      </h1>
+      <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place</p>
+      <ExploreBtn/>
 
-export default Page
+      <div id="events" className="mt-20 space-y-7">
+       <h3>Featured Events</h3>
+
+       <ul className="events">
+        {[1,2,3,4,5].map((event) => (
+           <li key={event}>Event {event}</li>
+        ))}
+       </ul>
+      </div>
+    </section> 
+  );
+};
+
+export default Page;
