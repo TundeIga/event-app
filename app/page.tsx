68ebe6b7d66b8e0209b7b15b23c610f61d49f417
Iga Tunde <igatunde06@gmail.com>
2025-11-07 01:44:01 +0100

    implement events card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 import ExploreBtn from "./components/ExploreBtn";
+import EventCard from "./components/EventCard";
+import events from "@/lib/constants";
+
+
 
 const Page = () => {
   return (
@@ -6,19 +10,23 @@ const Page = () => {
       <h1 className="text-center">
         The Hub For Every Dev <br /> Event You Can`t Miss
       </h1>
-      <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place</p>
-      <ExploreBtn/>
+      <p className="text-center mt-5">
+        Hackathons, Meetups and Conferences, All in One Place
+      </p>
+      <ExploreBtn />
 
       <div id="events" className="mt-20 space-y-7">
-       <h3>Featured Events</h3>
+        <h3>Featured Events</h3>
 
-       <ul className="events">
-        {[1,2,3,4,5].map((event) => (
-           <li key={event}>Event {event}</li>
-        ))}
-       </ul>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
       </div>
-    </section> 
+    </section>
   );
 };
 
