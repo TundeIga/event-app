8b1a9180eba1b24a1918d28bf92bef19c4fa3f68
Iga Tunde <igatunde06@gmail.com>
2025-11-17 20:09:30 +0100

    image uplaod hooks

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -2,6 +2,7 @@
 import Event from "@/app/database/event.model";
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
   try {
@@ -20,13 +21,35 @@ export async function POST(req: NextRequest) {
       );
     }
 
+    const file = formData.get("image") as File | null;
+    if (!file)
+      return NextResponse.json(
+        { message: "Image file is required" },
+        { status: 400 }
+      );
+
+      const arrayBuffer = await file.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+
+      const uploadResult = await new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream({resource_type: "image", folder: "DevEvents"}, (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        }).end(buffer);
+      })
+
+       event.image = (uploadResult as { secure_url: string }).secure_url;
+
     const createdEvent = await Event.create(event);
 
-    return NextResponse.json({
-      message: "Event created successfully",
-      event: createdEvent,
-    }, {status: 201});
-  } catch (e) { 
+    return NextResponse.json(
+      {
+        message: "Event created successfully",
+        event: createdEvent,
+      },
+      { status: 201 }
+    );
+  } catch (e) {
     console.error(e);
     return NextResponse.json(
       {
